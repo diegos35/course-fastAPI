83800b5343f97e10b10df13da7c4287bc438b284
diego <diego_1996.c.c@gmail.com>
2024-04-25 00:44:51 -0500

    feat: method get

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app =  FastAPI()
 app.title = 'Mi aplication con FastAPI'
@@ -7,4 +8,19 @@ app.version = "0.0.1"
 
 @app.get("/", tags=['home'])
 def message():
-    return  {"message": "Hello World!"}
+    return  HTMLResponse('ok')
+
+movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
+    }
+]
+
+@app.get("/movies",tags=['movies'])
+def get_movies():
+   return movies
