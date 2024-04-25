509e91521706e7caf2edb40d74c1e64f3df12c78
diego <diego_1996.c.c@gmail.com>
2024-04-25 00:35:49 -0500

    automatic documentation with swagger

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 app =  FastAPI()
+app.title = 'Mi aplication con FastAPI'
+app.version = "0.0.1"
 
-@app.get("/")
+
+@app.get("/", tags=['home'])
 def message():
     return  {"message": "Hello World!"}
