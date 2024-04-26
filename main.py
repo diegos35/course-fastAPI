3f7976163d449c7efdaf48f9b1010bbbb15caa79
diego <diego_1996.c.c@gmail.com>
2024-04-26 00:03:42 -0500

    create paramters of path

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ app.title = 'Mi aplication con FastAPI'
 app.version = "0.0.1"
 
 
-@app.get("/", tags=['home'])
-def message():
-    return  HTMLResponse('ok')
-
 movies = [
     {
         "id": 1,
@@ -18,9 +14,29 @@ movies = [
         "year": "2009",
         "rating": 7.8,
         "category": "Acción"
+    },
+      {
+        "id": 2,
+        "title": "Avatar2",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
+        "year": "2010",
+        "rating": 7.8,
+        "category": "Acción"
     }
 ]
+
+@app.get("/", tags=['home'])
+def message():
+    return  HTMLResponse('ok')
+
 
 @app.get("/movies",tags=['movies'])
 def get_movies():
    return movies
+
+
+@app.get("/movies/{id}", tags=["movies"])
+def get_movie(id: int):
+     return [movie for movie in movies if movie["id"] == id]
+    ##movie = next((movie for movie in movies if movie["id"] == id), [])
+    ##return movie
